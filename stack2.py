d5a0dd0ee89c2a83322b9fec321ef6d8439b7fb1
k.  Soumya Ranjan  Das <soumya292ranjan@gmail.com>
2026-07-07 13:26:43 +0530

    stack manupulation

---
+++ stack2.py
@@ -0,0 +1,12 @@
+stack = []
+stack.append(20)
+stack.append(30)
+stack.append(40)
+stack.append(50)
+
+
+store = stack.pop(), stack.pop(), stack.pop()
+print(store)
+stack.append(70)
+stack.append(80)
+print(stack)
