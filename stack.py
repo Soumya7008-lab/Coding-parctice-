8688bb17a5b8e0623502ae5d6ed61b324ed0fff8
k.  Soumya Ranjan  Das <soumya292ranjan@gmail.com>
2026-07-07 10:51:33 +0530

    Initial commit - adding practice codes

---
+++ stack.py
@@ -0,0 +1,10 @@
+stack = []
+
+stack.append("plate 1")
+stack.append("plate 2")
+stack.append("plate 3")
+print ("stack after pushing plates ", stack)
+
+top_plate = stack.pop()
+print("poped element", top_plate)
+print ("stack now", stack) 
